a29ef0d037e94fe007208f93756f6efcc3f8af85
Darren S. <sloppydj@gmail.com>
2025-07-14 17:42:19 -0400

    Updated files based on course requirements

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,46 @@
+// Sets the linebreak variable to make the output easier to read as each section is on it's own line
+var lineBreak = "<br>"
+
+// Assignment
+window.alert('Hello, World!!'); //Sends text to a pop-up window
+document.write('Hola, World!'); //Writes to the HTML page
+document.write(lineBreak); //sends a line break
+
+//String Assignment
+var A = "This is a string"; //sets the variable
+document.write(A); //writes the variable
+document.write(lineBreak); //sends a line break
+
+// Escape Character Assignment
+var x = "Here is a string to be displayed"; //sets the variable
+window.alert(x); //writes the variable to a pop-up box
+document.write("I told my famliy, \"It's fun to code!\"");
+document.write(lineBreak); //sends a line break
+
+//Concatenation Assignment
+document.write("\"Trust in the Lord with all your heart, and lean not on your own understanding;"
+    + " In all your ways acknowledge Him, And He shall direct your paths.\""
+    + " - Proverbs 3:5-6"
+);  //writes the phrase
+document.write(lineBreak); //sends a line break
+
+// Concatenated String Challenge
+var strA = "\"For we walk by faith, not by sight.\""; //sets the variable
+var srtB = "2 Corinthians 5:7"; //sets the variable
+var strC = " - "; //sets the variable
+document.write (strA + strC + srtB);  //concatenates the variables
+document.write(lineBreak); //sends a line break
+
+// Multiple Variables Assignment
+var baseball = "Rays", football = "Steelers", hockey = "Penguins", college = "Bearcats"; //sets multiple variables on a single line
+document.write (college); //writes one of those variables
+document.write(lineBreak); //sends a line break
+
+// Expression Assignment
+document.write(5 + 4); //writes the expression
+document.write(lineBreak); //sends a line break
+
+//BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT
+var Sent1 = "This is the beginning of the string"; //sets the variable
+var Sent2 = "and this is the end of the string"; //sets the variable
+document.write(Sent1 + Sent2); //concatenates the variables
